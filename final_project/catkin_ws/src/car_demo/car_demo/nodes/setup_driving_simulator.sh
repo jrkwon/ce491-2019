935ea011ccbe53b50de8c68f0f009b68f4d73ae3
bimi-lab <bimilab.dearborn@gmail.com>
2020-12-06 21:18:24 -0500

    New launch file for positioning car at an S turn

--- final_project/catkin_ws/src/car_demo/car_demo/nodes/setup_driving_simulator.sh
+++ final_project/catkin_ws/src/car_demo/car_demo/nodes/setup_driving_simulator.sh
@@ -1,4 +1,4 @@
 # !/bin/bash
 
-sudo cp -a 046d:c261 /etc/usb_modeswitch.d/
-sudo usb_modeswitch -c /etc/usb_modeswitch.d/046d:c261
+sudo cp -a 046d:c262 /etc/usb_modeswitch.d/
+sudo usb_modeswitch -c /etc/usb_modeswitch.d/046d:c262
